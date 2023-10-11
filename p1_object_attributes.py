0f820a0e8a4ac789b5ab9cf4ad348e6b6e8cf17d
Ceslander <107605704+Ceslander@users.noreply.github.com>
2023-10-11 12:31:54 +0800

    union-find

--- p1_object_attributes.py
+++ p1_object_attributes.py
@@ -12,7 +12,39 @@ def binarize(gray_image, thresh_val):
 
 def label(binary_image):
     # TODO
-    return labeled_image
+   
+    length, width, channels = np.shape(binary_image)
+    parent = np.zeros((length, width))
+
+    # union-find set
+    def index(i, j):
+        # (i,j) to i*width+j.
+        assert i < length and j < width, "Illegal index."
+        return i*width+j
+    def coordinate(index):
+        # i*width+j to (i,j).
+        j = index % width
+        i = round((index-j)/width)
+        return i,j
+    def union_find_init(parent):
+        i = j = 0
+        while i < length:
+            parent[i][j] = index(i, j)
+            if j < width - 1:
+                j += 1
+            else:
+                i += 1
+                j = 0
+    def find(i, j):
+        if parent[i][j] == index(i, j):
+            return index(i,j)
+        else: 
+            parent_i, parent_j = coordinate(parent[i][j])
+            return find(parent_i, parent_j)
+    def merge(i, j, parent_i, parent_j):
+        parent[i][j] = index(parent_i, parent_j)
+
+    return labeled_imag
 
 
 def get_attribute(labeled_image):
